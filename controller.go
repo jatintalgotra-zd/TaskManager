316c3eb10196780d6f9cd901ffe3b491f79148c3
jatintalgotra-zd <jatin.talgotra@zop.dev>
2025-06-23 14:23:06 +0530

    Initial Commit

---
+++ controller.go
@@ -0,0 +1,118 @@
+package main
+
+import (
+	"encoding/json"
+	"io"
+	"net/http"
+	"strconv"
+)
+
+type Handler struct {
+	tm *TaskManager
+}
+
+func NewHandler(taskManager *TaskManager) *Handler {
+	return &Handler{tm: taskManager}
+}
+
+func (h *Handler) GetAll(w http.ResponseWriter, req *http.Request) {
+	if req.Method != http.MethodGet {
+		w.WriteHeader(http.StatusMethodNotAllowed)
+	} else {
+		pendingTasks := h.tm.ListTasks()
+
+		pBytes, err := json.Marshal(pendingTasks)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+
+		_, err2 := w.Write(pBytes)
+		if err2 != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+		}
+	}
+}
+
+func (h *Handler) GetByID(w http.ResponseWriter, req *http.Request) {
+	if req.Method != http.MethodGet {
+		w.WriteHeader(http.StatusMethodNotAllowed)
+	}
+
+	id := req.PathValue("id")
+
+	idInt, err := strconv.ParseInt(id, 10, 64)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+	}
+
+	task, err2 := h.tm.ListTaskByID(int(idInt))
+	if err2 != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	tBytes, err3 := json.Marshal(task)
+	if err3 != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	_, err4 := w.Write(tBytes)
+	if err4 != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+	}
+}
+
+func (h *Handler) PostTask(w http.ResponseWriter, req *http.Request) {
+	if req.Method != http.MethodPost {
+		w.WriteHeader(http.StatusMethodNotAllowed)
+	} else {
+		desc, err := io.ReadAll(req.Body)
+		if err != nil {
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+
+		task := string(desc)
+		h.tm.AddTask(task)
+		w.WriteHeader(http.StatusCreated)
+	}
+}
+
+func (h *Handler) PutTask(w http.ResponseWriter, req *http.Request) {
+	if req.Method != http.MethodPut {
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		return
+	}
+
+	id := req.PathValue("id")
+
+	idInt, err := strconv.ParseInt(id, 10, 64)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	h.tm.CompleteTask(int(idInt))
+}
+
+func (h *Handler) DeleteTask(w http.ResponseWriter, req *http.Request) {
+	if req.Method != http.MethodDelete {
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		return
+	}
+
+	id := req.PathValue("id")
+
+	idInt, err := strconv.ParseInt(id, 10, 64)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	err2 := h.tm.DeleteTaskByID(int(idInt))
+	if err2 != nil {
+		w.WriteHeader(http.StatusBadRequest)
+	}
+}
---
+++ main.go
@@ -0,0 +1,119 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"net/http"
+	"time"
+)
+
+var ErrInvalid = errors.New("invalid task id")
+
+type TaskManager struct {
+	tasks  []Task
+	nextID func() int
+}
+
+func NewTaskManager() *TaskManager {
+	return &TaskManager{tasks: make([]Task, 0), nextID: GenID()}
+}
+
+// Task struct
+// added fields for description and status.
+type Task struct {
+	Desc   string
+	Status bool
+}
+
+// GenID () func() int
+// returns function to get next id.
+func GenID() func() int {
+	id := 0
+
+	return func() int {
+		id++
+		return id
+	}
+}
+
+// AddTask (description string, nextID func() int, mp map[int]*Task)
+// adds new Task by generating id.
+func (t *TaskManager) AddTask(description string) int {
+	id := t.nextID()
+	t1 := Task{Desc: description}
+	t.tasks = append(t.tasks, t1)
+
+	fmt.Println("Task added:", id, "-", description)
+
+	return id
+}
+
+// ListTasks () []string
+// prints all pending tasks.
+func (t *TaskManager) ListTasks() []Task {
+	return t.tasks
+}
+
+func (t *TaskManager) ListTaskByID(id int) (Task, error) {
+	var err error
+
+	idx := id - 1
+	if idx < 0 || idx >= len(t.tasks) {
+		err = ErrInvalid
+		return Task{}, err
+	}
+
+	if t.tasks[idx].Status {
+		return Task{}, ErrInvalid
+	}
+
+	return t.tasks[idx], nil
+}
+
+func (t *TaskManager) DeleteTaskByID(id int) error {
+	idx := id - 1
+	if idx < 0 || idx >= len(t.tasks) {
+		fmt.Println("invalid task id for Delete method")
+		return ErrInvalid
+	}
+
+	t.tasks[idx] = t.tasks[len(t.tasks)-1]
+	t.tasks = t.tasks[:len(t.tasks)-1]
+
+	return nil
+}
+
+// CompleteTask (int)
+// marks Task complete by id.
+func (t *TaskManager) CompleteTask(id int) {
+	idx := id - 1
+	if idx >= 0 && idx < len(t.tasks) {
+		fmt.Println("Marking task", id, "as completed...")
+
+		(t.tasks)[idx].Status = true
+	} else {
+		fmt.Println("Invalid task ID for Complete method")
+	}
+}
+
+func main() {
+	taskManager := NewTaskManager()
+	handler := NewHandler(taskManager)
+	http.HandleFunc("GET /api/task", handler.GetAll)
+	http.HandleFunc("GET /api/task/{id}", handler.GetByID)
+	http.HandleFunc("POST /api/task", handler.PostTask)
+	http.HandleFunc("PUT /api/task/{id}", handler.PutTask)
+	http.HandleFunc("DELETE /api/task/{id}", handler.DeleteTask)
+
+	svr := http.Server{
+		Addr:              ":8000",
+		Handler:           http.DefaultServeMux,
+		ReadHeaderTimeout: 20 * time.Second,
+		IdleTimeout:       60 * time.Second,
+	}
+
+	err := svr.ListenAndServe()
+	if err != nil {
+		fmt.Println("Error starting server")
+	}
+}
